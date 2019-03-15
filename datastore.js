62842b977af59823155f5a80ba47adfb7f6fcc11
ChanceSharp <47234818+ChanceSharp@users.noreply.github.com>
2019-03-14 22:08:27 -0600

    Update datastore.js

--- datastore.js
+++ datastore.js
@@ -8,8 +8,8 @@ const STORE = [
         'Marc Wilson'
          ],
       correctAnswer: 'Gifford Nielsen',
-      icon: 'https://farm8.staticflickr.com/7318/9950191195_135598fef2_b.jpg',
-      alt: 'referees holding hands up'
+      icon: 'https://www.sltrib.com/resizer/4YsXunMUcyHWpomlGCe8huXHwLY=/0x600/smart/filters:quality(86)/arc-anglerfish-arc2-prod-sltrib.s3.amazonaws.com/public/DXKIOUBWSNBHPODCMDZTKXRXP4.JPG',
+      alt: 'football player flexing'
     },
     {
       question: 'In 2013, Taysom Hill recorded 4,282 yards of total offense. Prior to Hill, who was the last BYU QB to surpass the 4,000-yard mark in total offense?',
@@ -20,8 +20,8 @@ const STORE = [
         'Brandon Doman'
         ],
         correctAnswer: 'Max Hall',
-        icon: 'https://farm8.staticflickr.com/7318/9950191195_135598fef2_b.jpg',
-        alt: 'referees holding hands up'
+        icon: 'https://usatcfn.files.wordpress.com/2018/10/usatsi_11384976-2.jpg?w=1000&h=600&crop=1',
+        alt: 'football player throwing ball'
     },
     {
       question: "BYU's lowest career net rushing yardage tally is minus-654 yards, recorded by which BYU quarterback?",
@@ -32,8 +32,8 @@ const STORE = [
         'Kevin Feterik'
         ],
         correctAnswer: 'John Walsh',
-        icon: 'https://farm8.staticflickr.com/7318/9950191195_135598fef2_b.jpg',
-        alt: 'referees holding hands up'
+        icon: 'https://cdn-s3.si.com/s3fs-public/styles/marquee_large_2x/public/2018/09/24/byu-fly-sweep-offense-washington-wisconsin-jeff-grimes.jpg?itok=FKyorL7q',
+        alt: 'football player running'
     },
     {
       question: "BYU set its single-game record for total-offense yardage with 777 yards against ___________ in 1983.",
@@ -44,8 +44,8 @@ const STORE = [
         'New Mexico'
         ],
       correctAnswer: 'New Mexico',
-      icon: 'https://farm8.staticflickr.com/7318/9950191195_135598fef2_b.jpg',
-      alt: 'referees holding hands up'
+      icon: 'https://media.deseretdigital.com/file/028eb07062?resize=width_1200&type=jpg&c=6&a=e0717f4c',
+      alt: 'football player running'
     },
     {
       question: "Which BYU quarterback still holds the FBS single-game record for most yards per completion (min. 22 pass attempts)?",
@@ -56,8 +56,8 @@ const STORE = [
         'Steve Young'
         ],
       correctAnswer: 'John Walsh',
-      icon: 'https://farm8.staticflickr.com/7318/9950191195_135598fef2_b.jpg',
-      alt: 'referees holding hands up'
+      icon: 'https://cdn.vox-cdn.com/thumbor/Qr6sxHFgXx-ITpwT3bSvqtfDwbE=/0x0:3600x2400/1200x800/filters:focal(1512x912:2088x1488)/cdn.vox-cdn.com/uploads/chorus_image/image/62412712/usa_today_11729193.0.jpg',
+      alt: 'football player holding hands up'
     },
     {
       question: "BYU has scored 50+ points in 15 games during the Bronco Mendenhall era; the only such game with 60+ points came against _________.",
@@ -68,8 +68,8 @@ const STORE = [
         'UNLV'
         ],
       correctAnswer: 'Air Force',
-      icon: 'https://farm8.staticflickr.com/7318/9950191195_135598fef2_b.jpg',
-      alt: 'referees holding hands up'
+      icon: 'https://bloximages.chicago2.vip.townnews.com/heraldextra.com/content/tncms/assets/v3/editorial/3/83/383cea5f-16a3-5d9b-974e-dcb228b1d751/59a1faafcc224.image.jpg?resize=1200%2C1595',
+      alt: 'football player making a catch'
     },
     {
       question: "During the 2013 season's 40-21 win over Texas, in which BYU ran for a school-record 550 yards on 72 carries, how many passes did the Cougars complete?",
@@ -80,8 +80,8 @@ const STORE = [
       '17'
       ],
       correctAnswer: '9',
-      icon: 'https://farm8.staticflickr.com/7318/9950191195_135598fef2_b.jpg',
-      alt: 'referees holding hands up'
+      icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgiKhBDzmVNkOYa0DfnEcHyVnpfECj7pQ9ktqSiT9ZFvrweE9v',
+      alt: 'football player scoring touchdown'
     },
     {
       question: "Which is the only 'Power 5' program against which Bronco Mendenhall has multiple wins without a single loss?",
@@ -89,11 +89,11 @@ const STORE = [
         'Georgia Tech',
         'Oklahoma',
         'UCLA',
-        'Oregon State.'
+        'Oregon State'
       ],
       correctAnswer: 'Georgia Tech',
-      icon: 'https://farm8.staticflickr.com/7318/9950191195_135598fef2_b.jpg',
-      alt: 'referees holding hands up'
+      icon: 'https://utahutes.com/images/2019/1/14/Defense_Line_of_Scrimmage_BYU_11_24_18_12.jpg',
+      alt: 'football players blocking'
     },
     {
       question: "Who was the last BYU placekicker to make a field goal of 50+ yards?",
@@ -104,8 +104,8 @@ const STORE = [
       'Mitch Payne'
       ],
       correctAnswer: 'Jared McLaughlin',
-      icon: 'https://farm8.staticflickr.com/7318/9950191195_135598fef2_b.jpg',
-      alt: 'referees holding hands up'
+      icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8FqGgptBPaonuRl8R2-erlIn-Q-KbGp-MRtBItdWRsTNkIWiT',
+      alt: 'football player holding hands up'
     },
     {
       question: "Who was the first BYU player to record multiple seasons with 1,000+ receiving yards?",
@@ -116,8 +116,7 @@ const STORE = [
         'Chris Smith'
     ],
     correctAnswer: 'Chris Smith',
-    icon: 'https://farm8.staticflickr.com/7318/9950191195_135598fef2_b.jpg',
-    alt: 'referees holding hands up'
+    icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ29dgCO-zOQrLcFkeIEUiUxgliRHrVTOzoAbdCx3JbzGL_5vOH',
+    alt: 'mascot pointing'
     }
 ];
-
